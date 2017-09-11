92bcb7ba612470ada2f11ab2cbdffefa3851e9d2
박다정 <daaa08@naver.com>
2017-09-12 00:12:51 +0900

    youtube player

---
+++ app/src/main/java/com/da08/youtube2/DeveloperKey.java
@@ -0,0 +1,9 @@
+package com.da08.youtube2;
+
+/**
+ * Created by Da08 on 2017. 9. 11..
+ */
+
+public class DeveloperKey {
+    public static final String DEVELOPER_KEY = "AIzaSyCHwJB1yZI1rQpxcvpXX9VN0XIkokQzxdE";
+}
--- app/src/main/java/com/da08/youtube2/MainActivity.java
+++ app/src/main/java/com/da08/youtube2/MainActivity.java
@@ -1,13 +1,57 @@
 package com.da08.youtube2;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubeApiServiceUtil;
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class MainActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener{
+
+    private static final int RECOVERY_DIALOG_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("youtube Test", "사용가능여부:"+ YouTubeApiServiceUtil.isYouTubeApiServiceAvailable(this)); // 성공
+        getYouTubePlayerProvider().initialize(DeveloperKey.DEVELOPER_KEY,this);
+
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+
+        youTubePlayer.cueVideo("IA1hox-v0jQ");
+        //지정한 동영상의 미리보기 이미지를 로드하고 플레이어가 동영상을 재생하도록 준비하지만 play()를 호출하기 전에는 동영상 스트림을 다운로드하지 않음.
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult errorReason) {
+            if (errorReason.isUserRecoverableError()) {
+                errorReason.getErrorDialog(this, RECOVERY_DIALOG_REQUEST).show();
+            } else {
+                String errorMessage = String.format(
+                        getString(R.string.error_player), errorReason.toString());
+                Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show();
+            }
+        }
+
+    protected YouTubePlayer.Provider getYouTubePlayerProvider() {
+        return (YouTubePlayerView) findViewById(R.id.youtubeView);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RECOVERY_DIALOG_REQUEST) {
+            // Retry initialization if user performed a recovery action
+            getYouTubePlayerProvider().initialize(DeveloperKey.DEVELOPER_KEY, this);
+        }
     }
 }
